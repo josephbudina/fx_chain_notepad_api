15d5ae4689fef0c040bec43b7a41a7c2fda709c9
Joseph Budina <josephbudina@Mac.lan>
2025-09-05 13:05:57 -0700

    Add faker

---
+++ spec/factories/notes.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :notes do
+    id { "John" }
+    title { "Doe" }
+    body { false }
+  end
+end
--- spec_helper.rb
+++ spec_helper.rb
@@ -5,6 +5,10 @@ require 'pry'
 require 'factory_bot'
 
 RSpec.configure do |config|
-  config.include FactoryBot::Syntax::Methods
+	config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    FactoryBot.find_definitions
+  end
 end
 
